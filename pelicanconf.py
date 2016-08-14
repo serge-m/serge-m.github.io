03ffb7043d515caf9d09f31363c57e73f049682e
Sergey Matuynin <sbmatyunin@gmail.com>
2016-08-15 00:09:57 +0200

    Switched to another theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -6,7 +6,7 @@ AUTHOR = 'sergem'
 SITENAME = 'Personal public notebook'
 SITEURL = ''
 
-THEME='pelican-twitchy'
+THEME='pelican-bootstrap3'
 
 PATH = 'content'
 USE_FOLDER_AS_CATEGORY = False
@@ -26,13 +26,23 @@ AUTHOR_FEED_RSS = None
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
          ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         # ('You can modify those links in your config file', '#'),
+         )
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+# # Social widget
+# SOCIAL = (('You can add links in your config file', '#'),
+#           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = True
+DEFAULT_PAGINATION = 20
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+
+MAIN_MENU=True
+
+DISPLAY_CATEGORIES_ON_SIDEBAR=True
+RECENT_POST_COUNT=10
+DISPLAY_RECENT_POSTS_ON_SIDEBAR=True
+DISPLAY_TAGS_ON_SIDEBAR=True
+DISPLAY_PAGES_ON_MENU=True
